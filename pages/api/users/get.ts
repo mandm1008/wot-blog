c61a636760fe8b31b0d914432f3e10193a3e1159
mandm1008 <dominhmanupwork1306@gmail.com>
2022-10-26 09:44:13 +0700

    add secure

--- pages/api/users/get.ts
+++ pages/api/users/get.ts
@@ -48,7 +48,7 @@ export default nc({
           cookie.serialize('refreshToken', refreshToken, {
             httpOnly: true,
             path: '/',
-            // secure: true,
+            secure: true,
             sameSite: 'strict'
           })
         )
